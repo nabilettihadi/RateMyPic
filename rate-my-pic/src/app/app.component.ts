91325b8a1a4018da7c7bccf8bb143b96da62740f
Nabil Ettihadi <nabilettihadi.ma@gmail.com>
2025-03-07 10:35:59 +0000

    feat: Configure main app component and NgRx store integration

--- rate-my-pic/src/app/app.component.ts
+++ rate-my-pic/src/app/app.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ImageGalleryComponent } from './components/image-gallery/image-gallery.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, ImageGalleryComponent],
+  template: `
+    <div class="min-h-screen bg-gray-100">
+      <app-image-gallery></app-image-gallery>
+    </div>
+  `,
+  styles: [`
+    :host {
+      display: block;
+    }
+  `]
 })
 export class AppComponent {
   title = 'rate-my-pic';
--- rate-my-pic/src/app/app.config.ts
+++ rate-my-pic/src/app/app.config.ts
@@ -1,12 +1,26 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { imageReducer } from './store/reducers/image.reducer';
+import { ImageEffects } from './store/effects/image.effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(),
+    provideStore({ images: imageReducer }),
+    provideEffects([ImageEffects]),
+    provideStoreDevtools({
+      maxAge: 25,
+      logOnly: !isDevMode(),
+      autoPause: true,
+      trace: false,
+      traceLimit: 75,
+    }),
   ]
 };
